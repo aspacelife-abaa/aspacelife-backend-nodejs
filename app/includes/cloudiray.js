d95bbb18048385c2fe95da1b89f4707819f63bbf
Marshalsoft <marshallekene@hotmail.com>
2023-04-14 13:50:22 +0100

    upload isses

--- app/includes/cloudiray.js
+++ app/includes/cloudiray.js
@@ -1,25 +1,47 @@
+const { default: Axios } = require('axios');
 require('dotenv').config();
-const {Cloudinary} = require('@cloudinary/url-gen');
+import {Cloudinary} from '@cloudinary/url-gen';
 
 const x = {
     api_key:process.env.CLOUDINARY_api_key,
     api_secret:process.env.CLOUDINARY_api_secret,
     cloud_name:process.env.CLOUDINARY_cloud_name
 }
-const cldInstance = new Cloudinary({cloud: {cloudName: 'dcqt9xiub'}});
 
 console.log(x);
 const CloudinaryCall = (params)=>{
 return new Promise((resolve)=>{
-    const fetchedImage = cldInstance.image('https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg').setDeliveryType('fetch');
-            console.log(fetchedImage.toURL())
+const url = `https://api.cloudinary.com/v1_1/${x.cloudName}/image/upload`;
+let data = new FormData();
+data.append('file',params.imageString);
+// data.append('upload_preset',`${x.api_key}`);
+data.append('public_id',`social_feeds`);
+data.append('api_key',`${x.api_key}`);
+
+let config = {
+  method: 'post',
+  url:url,
+  headers: { 
+    'api_key':`${x.api_key}`,
+    'Content-Type':'application/json'
+  },
+  data:data
+};
+ Axios.post(config).then((res)=>{
+            console.log(res)
             resolve({
                 status:true,
                 message:"",
-                data:{
-                    uri:fetchedImage.toURL()
-                }
+                data:{}
             })
+        }).catch((res)=>{
+            console.log(res)
+            resolve({
+                status:true,
+                message:"",
+                data:{}
+            })   
+        })
     })
 }
 module.exports = {CloudinaryCall};
--- app/routes/regular/post_feeds.js
+++ app/routes/regular/post_feeds.js
@@ -1,14 +1,14 @@
 const {version} = require('../../includes/config');
-const {CloudinaryCall} = require('../../includes/cloudiray');
+// const {CloudinaryCall} = require('../../includes/cloudiray');
 module.exports = (app)=>{
     app.post(`/${version}/create_social_feed`,(req, res) =>{
         let params  = Object.assign({token:req.headers["token"]},req.body);
-        CloudinaryCall(params).then((data) => {
-            params.imageString = JSON.stringify(data)
-            console.log(params);
+        // CloudinaryCall(params).then((data) => {
+        //     params.imageString = JSON.stringify(data)
+        //     console.log(params);
             req.BaseFunctions.PostSocialFeed(params).then((response)=>{
              res.json(response);
             })
-        })
+        // })
     })
 }
