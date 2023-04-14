ca136e71520d6c0516f05e65a025e3b673a05d74
Marshalsoft <marshallekene@hotmail.com>
2023-04-14 06:45:34 +0100

    kkkk

--- app/routes/regular/post_feeds.js
+++ app/routes/regular/post_feeds.js
@@ -1,21 +1,21 @@
 const {version} = require('../../includes/config');
-const cloudary = require('../../includes/cloudiray');
+// const cloudary = require('../../includes/cloudiray');
 module.exports = (app)=>{
     app.post(`/${version}/create_social_feed`,(req, res) =>{
         let params  = Object.assign({token:req.headers["token"]},req.body);
-        const clres = cloudary.uploader.upload('https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg',{
-         public_id: "social_feed",
-         folder:"social_feed",
-        })
-        clres.then((data) => {
-            params.imageString = JSON.stringify({
-            id:data.public_id,
-            url:data.secure_url
-            })
-            console.log(params);
+        // const clres = cloudary.uploader.upload('https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg',{
+        //  public_id: "social_feed",
+        //  folder:"social_feed",
+        // })
+        // clres.then((data) => {
+        //     params.imageString = JSON.stringify({
+        //     id:data.public_id,
+        //     url:data.secure_url
+        //     })
+        //     console.log(params);
             req.BaseFunctions.PostSocialFeed(params).then((response)=>{
              res.json(response);
             })
-        })
+        // })
     })
 }
