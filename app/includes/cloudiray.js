50a2644f69d8d03d1d98420f5819a304d7284256
Marshalsoft <marshallekene@hotmail.com>
2023-04-14 13:37:00 +0100

    upload isses

--- app/includes/cloudiray.js
+++ app/includes/cloudiray.js
@@ -1,10 +1,25 @@
-const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
+const {Cloudinary} = require('@cloudinary/url-gen');
+
 const x = {
     api_key:process.env.CLOUDINARY_api_key,
     api_secret:process.env.CLOUDINARY_api_secret,
     cloud_name:process.env.CLOUDINARY_cloud_name
 }
+const cldInstance = new Cloudinary({cloud: {cloudName: 'dcqt9xiub'}});
+
 console.log(x);
-cloudinary.config(x);
-module.exports = cloudinary;
+const CloudinaryCall = (params)=>{
+return new Promise((resolve)=>{
+    const fetchedImage = cldInstance.image('https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg').setDeliveryType('fetch');
+            console.log(fetchedImage.toURL())
+            resolve({
+                status:true,
+                message:"",
+                data:{
+                    uri:fetchedImage.toURL()
+                }
+            })
+    })
+}
+module.exports = {CloudinaryCall};
