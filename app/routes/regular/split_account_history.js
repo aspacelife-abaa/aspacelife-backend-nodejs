6367608acfc5f06dc4ea7f4b0f97b056de00f637
Marshalsoft <marshallekene@hotmail.com>
2023-04-19 15:10:19 +0100

    split_payment_history

--- app/routes/regular/split_account_history.js
+++ app/routes/regular/split_account_history.js
@@ -1,6 +1,6 @@
 const {version} = require('../../includes/config');
 module.exports = (app)=>{
-    app.post(`/${version}/create_payment_history`,(req, res) =>{
+    app.post(`/${version}/split_payment_history`,(req, res) =>{
         const params = Object.assign({token:req.headers["token"]},req.body);
         req.BaseFunctions.SplitAccountHistory(params).then((response)=>{
         res.json(response);
