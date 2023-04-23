b4d4b91ad10922a06de07e32cec6688b8a7477f8
Marshalsoft <marshallekene@hotmail.com>
2023-04-23 01:53:31 +0100

    addition of platform

--- app/routes/regular/linkaccount.js
+++ app/routes/regular/linkaccount.js
@@ -2,13 +2,14 @@
 const {version} = require('../../includes/config');
 module.exports = (app)=>{
     app.post(`/${version}/link_bank_account`,(req, res) =>{
-        const params = Object.assign({token:req.headers["token"]},req.body);
+        let params = Object.assign({token:req.headers["token"],platform:req.headers["platform"]},req.body);
        if(params.platform == "android" || params.platform == "ios")
        {
         req.BaseFunctions.LinkAccountMobile(params).then((response)=>{
           res.json(response);
         })
        }else{
+        delete params.platform;
         req.BaseFunctions.LinkAccount(params).then((response)=>{
         res.json(response);
         })
