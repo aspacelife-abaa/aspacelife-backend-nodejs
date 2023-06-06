eda33eb8192084251c4826ea3fc375686c8a28e3
Marshalsoft <marshallekene@hotmail.com>
2023-06-06 17:09:27 +0100

    img

--- app/routes/regular/nin_verification_image.js
+++ app/routes/regular/nin_verification_image.js
@@ -1,6 +1,6 @@
 const {version} = require('../../includes/config');
 module.exports = (app)=>{
-    app.post(`/${version}/nin_verification_image`,(req, res) =>{
+    app.post(`/${version}/nin_verification_img`,(req, res) =>{
         const params  = Object.assign({token:req.headers["token"]},req.body);
         req.BaseFunctions.NINVerificationImage(params).then((response)=>{
         res.json(response);
