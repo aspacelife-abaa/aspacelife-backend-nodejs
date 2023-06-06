d6aa73ce9a02cc61e19866ad48b9e9564a11f593
Marshalsoft <marshallekene@hotmail.com>
2023-06-06 16:56:33 +0100

    Upfate

--- app/includes/NINPrembly/index.js
+++ app/includes/NINPrembly/index.js
@@ -1,5 +1,5 @@
 
-const axios = require('axios');
+var https = require('follow-redirects').https;
 const {SMSBaseUrl,SMSFolder,SenderID,FlutterWaveTestSecret} = require('../config');
 const FormData = require('form-data');
 
@@ -8,34 +8,54 @@ const NINImageVerification = async(data)=>{
     return response;
 }
 async function  Post(data){
-let fdata = new FormData();
-fdata.append('number', data.nin);
-fdata.append('image',data.image);
-console.log("<<data>>:",data);
+    var options = {
+        'method': 'POST',
+        'hostname': 'api.prembly.com',
+        'path': '/identitypass/verification/nin_w_face',
+        'headers': {
+          'X-Api-Key': 'sandbox_sk_nFpcm8RrZ4ZiMgsDFCFsU8h6yg9MhuKTkzKjgTz',
+          'app_id': 'b9c83b51-a22a-4ff0-a9ec-0d1f7e4c0d53'
+        },
+        'maxRedirects': 20
+      };
+      
     return new Promise((resolve)=>{
-        let config = {
-            method: 'post',
-            url: 'https://api.prembly.com/identitypass/verification/nin_w_face',
-            headers: { 
-              'X-Api-Key': 'sandbox_sk_nFpcm8RrZ4ZiMgsDFCFsU8h6yg9MhuKTkzKjgTz', 
-              'app_id': 'b9c83b51-a22a-4ff0-a9ec-0d1f7e4c0d53',
-              'accept': 'application/json',
-              'content-type': 'application/x-www-form-urlencoded'
-            },
-            data : fdata
-          };
-          axios.request(config).then((response) => {
-            console.log(JSON.stringify(response.data));
-            resolve(JSON.stringify(response.data))
-          })
-          .catch((error) => {
-            console.error(error);
-            resolve({
-            status:false,
-            message:error.message,
-            data:{}
-            })
+        var req = https.request(options, function (res) {
+            var chunks = [];
+          
+            res.on("data", function (chunk) {
+              chunks.push(chunk);
+            });
+          
+            res.on("end", function (chunk) {
+              var body = Buffer.concat(chunks);
+              let d = JSON.parse(body.toString());
+              if(d.detail)
+              {
+                d.message = d.detail;
+                delete d.detail;
+              }
+              if(d.response_code)
+              {
+                d.data = {
+                    response_code:d.response_code
+                }
+                delete d.response_code;
+              }
+              console.log(body.toString());
+              resolve(d);
+            });
+          
+            res.on("error", function (error) {
+              console.error(error);
+              resolve(error)
+            });
           });
+          
+          var postData = `------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"number\"\r\n\r\n${data.nin}\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"image\"\r\n\r\n${data.image}\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--`;
+          req.setHeader('content-type', 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW');
+          req.write(postData);
+          req.end();
         })
     }
     module.exports = {NINImageVerification};
