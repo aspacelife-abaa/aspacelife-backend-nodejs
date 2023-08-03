9c02ba9b298241c7494164dca3858f76037370bd
Marshalsoft <marshallekene@hotmail.com>
2023-08-03 16:10:49 +0100

    users

--- app/routes/regular/users.js
+++ app/routes/regular/users.js
@@ -2,8 +2,7 @@
 const {version} = require('../../includes/config');
 module.exports = (app)=>{
     app.post(`/${version}/valid_users`,(req, res) =>{
-        const params  = Object.assign({token:req.headers["token"]},req.body);
-        req.BaseFunctions.ValidUSERS(params).then((response)=>{
+        req.BaseFunctions.ValidUSERS().then((response)=>{
         res.json(response);
         })
     })
