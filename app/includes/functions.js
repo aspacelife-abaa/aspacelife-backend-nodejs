f8349d4644900ea8e62432394a43a8a818f3321a
Marshalsoft <marshallekene@hotmail.com>
2023-08-07 11:02:51 +0100

    force update

--- app/includes/functions.js
+++ app/includes/functions.js
@@ -5969,7 +5969,7 @@ const ValidUSERS = (d)=>{
 
 const ForceUpdate = (d)=>{
   return new Promise((resolve) => {
-    return {status:true,data:"4.0.0",message:""}
+    resolve({status:true,data:"4.0.0",message:""})
   })
 }
 module.exports = {
